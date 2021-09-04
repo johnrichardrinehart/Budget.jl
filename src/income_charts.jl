be70511301ad8e46a662e52eced4a24ef07c0493
John Rinehart <johnrichardrinehart@gmail.com>
2021-09-04 19:03:12 +0300

    chore: remove some debug changes

--- src/income_charts.jl
+++ src/income_charts.jl
@@ -23,26 +23,23 @@ struct Budget
 end
 
 function compound(dep::Deposit, t::Dates.DateTime, apr::Float64)
-#	if t<dep.when
-#		return 0
-#	end
-#	dep.amount*(1+apr)^(Dates.year(t-dep.when))
+	if t<dep.when
+		return 0
+	end
+	dep.amount*(1+apr)^(Dates.year(t-dep.when))
 end
 
 function compound(dep::RegularDeposit, t::Dates.DateTime, apr::Float64)
-#	if t<dep.every
-#		return 0
-#	end
-#	dep.amount*(1+apr)^(Dates.year(t-dep.every))
+	if t<dep.every
+		return 0
+	end
+	dep.amount*(1+apr)^(Dates.year(t-dep.every))
 end
 
 function portfolio_timeline(budget::Budget, ts::AbstractVector{Dates.DateTime}, apr::Float64)
 	deps = budget.deposits
 
-	Printf.@printf "\nI'm a %s\n" typeof(deps[1])
-
-	compound.(deps[1], ts, apr)
-#	component_growth = [ compound.(d, ts, apr) for d in deps ];
+	component_growth = [ compound.(d, ts, apr) for d in deps ];
 	r = sum(component_growth, dims=2)
 	return r
 end
